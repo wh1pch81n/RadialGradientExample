46d0dfb75de0a4a2225b743d9646ef2b5769d260
Ho, Derrick <derrick.ho@medtronic.com>
2015-06-08 17:29:46 -0700

    Initial Commit

---
+++ RadialGradientTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RadialGradientTest
+//
+//  Created by Ho, Derrick on 6/8/15.
+//  Copyright (c) 2015 Ho, Derrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RadialGradientTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RadialGradientTest
+//
+//  Created by Ho, Derrick on 6/8/15.
+//  Copyright (c) 2015 Ho, Derrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
